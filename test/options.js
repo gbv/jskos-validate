5b2d75e95ca844be11f45958af4199033d188f4f
Jakob Voss <voss@gbv.de>
2019-12-05 08:30:07 +0100

    Add option unknownFields

--- test/options.js
+++ test/options.js
@@ -2,7 +2,10 @@ const assert = require("assert")
 const validate = require("../lib/validate")
 
 describe("Options", () => {
-  it("allows custom properties by default", () => {
+  it("allows custom fields by default", () => {
     assert.ok(validate.concept({FOO:1}))
+  })
+  it("allows unknown fields if enabled", () => {      
+    assert.ok(validate.concept({x:1}, {unknownFields: true}))
   }) 
 }) 
